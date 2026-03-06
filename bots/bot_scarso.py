310c3769f2c7a33fa0a75553744e7e7f17f30634
lorenzocrippa314 <lorenzo.crippa314@gmail.com>
2026-03-06 23:32:29 +0100

    Delete bots/bot_scarso.py
    cancella bot_scarso

--- bots/bot_scarso.py
+++
@@ -1,9 +0,0 @@
-class Bot_Scarso:
-    def __init__(self):
-        pass
-
-    def start_game(self,briscola):
-        self.briscola=briscola
-
-    def make_move(self, carte, carte_prese, cartagiocata=None):
-        pass
